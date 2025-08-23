02d859abbcc4bc9a45d0274028e0ce43b01ba11f
AmeetR <ameetrahane@gmail.com>
2025-08-23 04:21:52 +0100

    Tighten finetune quickstart scaffolding

---
+++ micrographonia/__init__.py
@@ -0,0 +1,23 @@
+"""Compatibility wrapper exposing the ``symphonia`` package as ``micrographonia``.
+
+This allows documentation examples like ``python -m micrographonia.finetune.cli``
+to work while the underlying package is still named ``symphonia``.
+"""
+from symphonia import *  # noqa: F401,F403
+import sys as _sys
+import symphonia as _symphonia
+import symphonia.finetune as _finetune
+import symphonia.sdk as _sdk
+import symphonia.registry as _registry
+import symphonia.training as _training
+import symphonia.tools as _tools
+import symphonia.runtime as _runtime
+
+# Re-export common subpackages so ``micrographonia.<mod>`` maps to
+# ``symphonia.<mod>``.
+_sys.modules[__name__ + ".finetune"] = _finetune
+_sys.modules[__name__ + ".sdk"] = _sdk
+_sys.modules[__name__ + ".registry"] = _registry
+_sys.modules[__name__ + ".training"] = _training
+_sys.modules[__name__ + ".tools"] = _tools
+_sys.modules[__name__ + ".runtime"] = _runtime
